f7fe5a7d6c176fdf260f3b8067e6c0fbf0703ac3
一笑轮回 <982242260@qq.com>
2024-05-21 21:21:24 +0800

    *优化三角评分控件

--- app/src/main/java/com/yxlh/androidxy/demo/ui/rating/TriangleRatingAnimView.kt
+++ app/src/main/java/com/yxlh/androidxy/demo/ui/rating/TriangleRatingAnimView.kt
@@ -14,7 +14,6 @@ import androidx.core.graphics.ColorUtils
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import com.yxlh.androidxy.R
 
-
 fun Context.dpToPx(dp: Float): Float {
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
 }
@@ -71,45 +70,61 @@ class TriangleRatingAnimView @JvmOverloads constructor(
     private var animatedLeftRating = 0
     private var animatedRightRating = 0
     private var enableAnimation: Boolean = true
+    private var circleRadius: Float = context.dpToPx(5f)
+    private var padding: Float = 0f
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        color = strokeColor
-        strokeWidth = this@TriangleRatingAnimView.strokeWidth
     }
 
     private val outerPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        color = ratingStrokeColor
-        strokeWidth = this@TriangleRatingAnimView.ratingStrokeWidth
     }
 
     private val fillPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = ColorUtils.setAlphaComponent(ratingStrokeColor, (0.3 * 255).toInt())
     }
     private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        color = ratingStrokeColor
-        strokeWidth = context.dpToPx(1.5f)
     }
     private val circleFillPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = Color.WHITE
     }
 
+    private val path = Path()
+    private val ratingPath = Path()
+
     init {
         context.withStyledAttributes(attrs, R.styleable.TriangleRatingAnimView) {
+            enableAnimation = getBoolean(R.styleable.TriangleRatingAnimView_enableAnimation, true)
             maxRating = getInt(R.styleable.TriangleRatingAnimView_maxRating, 5)
+            strokeColor = getColor(R.styleable.TriangleRatingAnimView_strokeColor, Color.GRAY)
+            strokeWidth = getDimension(R.styleable.TriangleRatingAnimView_strokeWidth, context.dpToPx(2f))
+            ratingStrokeColor = getColor(R.styleable.TriangleRatingAnimView_ratingStrokeColor, Color.RED)
+            ratingStrokeWidth = getDimension(R.styleable.TriangleRatingAnimView_ratingStrokeWidth, context.dpToPx(4f))
+            circleRadius = getDimension(R.styleable.TriangleRatingAnimView_circleRadius, context.dpToPx(5f))
             upRating = getInt(R.styleable.TriangleRatingAnimView_upRating, 0)
             leftRating = getInt(R.styleable.TriangleRatingAnimView_leftRating, 0)
             rightRating = getInt(R.styleable.TriangleRatingAnimView_rightRating, 0)
-            strokeColor = getColor(R.styleable.TriangleRatingAnimView_strokeColor, Color.GRAY)
-            strokeWidth = context.dpToPx(getDimension(R.styleable.TriangleRatingAnimView_strokeWidth, 2f))
-            ratingStrokeColor = getColor(R.styleable.TriangleRatingAnimView_ratingStrokeColor, Color.RED)
-            ratingStrokeWidth = context.dpToPx(getDimension(R.styleable.TriangleRatingAnimView_ratingStrokeWidth, 4f))
-            enableAnimation = getBoolean(R.styleable.TriangleRatingAnimView_enableAnimation, true)
         }
+
+        padding = circleRadius + context.dpToPx(2f)
+
+        paint.color = strokeColor
+        paint.strokeWidth = strokeWidth
+
+        outerPaint.color = ratingStrokeColor
+        outerPaint.strokeWidth = ratingStrokeWidth
+
+        fillPaint.color = ColorUtils.setAlphaComponent(ratingStrokeColor, (0.3 * 255).toInt())
+
+        circlePaint.color = ratingStrokeColor
+        circlePaint.strokeWidth = ratingStrokeWidth
+
+        animatedUpRating = upRating
+        animatedLeftRating = leftRating
+        animatedRightRating = rightRating
     }
 
     private fun animateRating() {
@@ -131,53 +146,59 @@ class TriangleRatingAnimView @JvmOverloads constructor(
         super.onDraw(canvas)
         val width = measuredWidth.toFloat()
         val height = measuredHeight.toFloat()
-        val circleRadius = context.dpToPx(5f)
-        val padding = circleRadius + context.dpToPx(2f)
 
-        val p1 = width / 2 to padding
-        val p2 = padding to height - padding
-        val p3 = width - padding to height - padding
+        val p1 = floatArrayOf(width / 2, padding)
+        val p2 = floatArrayOf(padding, height - padding)
+        val p3 = floatArrayOf(width - padding, height - padding)
 
         // 绘制外部三角形
-        val path = Path().apply {
-            moveTo(p1.first, p1.second)
-            lineTo(p2.first, p2.second)
-            lineTo(p3.first, p3.second)
-            close()
-        }
+        path.reset()
+        path.moveTo(p1[0], p1[1])
+        path.lineTo(p2[0], p2[1])
+        path.lineTo(p3[0], p3[1])
+        path.close()
         canvas.drawPath(path, paint)
 
-        val centroidX = (p1.first + p2.first + p3.first) / 3
-        val centroidY = (p1.second + p2.second + p3.second) / 3
+        val centroidX = (p1[0] + p2[0] + p3[0]) / 3
+        val centroidY = (p1[1] + p2[1] + p3[1]) / 3
 
         // 绘制顶点到重心的连线
-        canvas.drawLine(p1.first, p1.second, centroidX, centroidY, paint)
-        canvas.drawLine(p2.first, p2.second, centroidX, centroidY, paint)
-        canvas.drawLine(p3.first, p3.second, centroidX, centroidY, paint)
+        canvas.drawLine(p1[0], p1[1], centroidX, centroidY, paint)
+        canvas.drawLine(p2[0], p2[1], centroidX, centroidY, paint)
+        canvas.drawLine(p3[0], p3[1], centroidX, centroidY, paint)
 
-        val dynamicP1 =
-            centroidX + (p1.first - centroidX) * (animatedUpRating / maxRating.toFloat()) to centroidY + (p1.second - centroidY) * (animatedUpRating / maxRating.toFloat())
-        val dynamicP2 =
-            centroidX + (p2.first - centroidX) * (animatedLeftRating / maxRating.toFloat()) to centroidY + (p2.second - centroidY) * (animatedLeftRating / maxRating.toFloat())
-        val dynamicP3 =
-            centroidX + (p3.first - centroidX) * (animatedRightRating / maxRating.toFloat()) to centroidY + (p3.second - centroidY) * (animatedRightRating / maxRating.toFloat())
+        val dynamicP1 = floatArrayOf(
+            centroidX + (p1[0] - centroidX) * (animatedUpRating / maxRating.toFloat()),
+            centroidY + (p1[1] - centroidY) * (animatedUpRating / maxRating.toFloat())
+        )
+        val dynamicP2 = floatArrayOf(
+            centroidX + (p2[0] - centroidX) * (animatedLeftRating / maxRating.toFloat()),
+            centroidY + (p2[1] - centroidY) * (animatedLeftRating / maxRating.toFloat())
+        )
+        val dynamicP3 = floatArrayOf(
+            centroidX + (p3[0] - centroidX) * (animatedRightRating / maxRating.toFloat()),
+            centroidY + (p3[1] - centroidY) * (animatedRightRating / maxRating.toFloat())
+        )
 
         // 绘制内部动态三角形
-        val ratingPath = Path().apply {
-            moveTo(dynamicP1.first, dynamicP1.second)
-            lineTo(dynamicP2.first, dynamicP2.second)
-            lineTo(dynamicP3.first, dynamicP3.second)
-            close()
-        }
+        ratingPath.reset()
+        ratingPath.moveTo(dynamicP1[0], dynamicP1[1])
+        ratingPath.lineTo(dynamicP2[0], dynamicP2[1])
+        ratingPath.lineTo(dynamicP3[0], dynamicP3[1])
+        ratingPath.close()
         canvas.drawPath(ratingPath, outerPaint)
         canvas.drawPath(ratingPath, fillPaint)
 
         // 绘制动态点上的空心圆
-        canvas.drawCircle(dynamicP1.first, dynamicP1.second, circleRadius, circlePaint)
-        canvas.drawCircle(dynamicP1.first, dynamicP1.second, circleRadius - context.dpToPx(1.5f), circleFillPaint)
-        canvas.drawCircle(dynamicP2.first, dynamicP2.second, circleRadius, circlePaint)
-        canvas.drawCircle(dynamicP2.first, dynamicP2.second, circleRadius - context.dpToPx(1.5f), circleFillPaint)
-        canvas.drawCircle(dynamicP3.first, dynamicP3.second, circleRadius, circlePaint)
-        canvas.drawCircle(dynamicP3.first, dynamicP3.second, circleRadius - context.dpToPx(1.5f), circleFillPaint)
+        if (upRating != 0 || leftRating != 0 || rightRating != 0) {
+            drawCircle(canvas, dynamicP1)
+            drawCircle(canvas, dynamicP2)
+            drawCircle(canvas, dynamicP3)
+        }
+    }
+
+    private fun drawCircle(canvas: Canvas, point: FloatArray) {
+        canvas.drawCircle(point[0], point[1], circleRadius, circlePaint)
+        canvas.drawCircle(point[0], point[1], circleRadius - context.dpToPx(1.5f), circleFillPaint)
     }
 }
